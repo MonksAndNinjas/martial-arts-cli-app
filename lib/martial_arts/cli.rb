0b127dab576fb98c860300bc957d147121a136de
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-20 21:53:57 +0000

    Added to call method

--- lib/martial_arts/cli.rb
+++ lib/martial_arts/cli.rb
@@ -3,6 +3,9 @@ class MartialArts::CLI
 
   def call
     puts "OOOOOOSSSsss"
+    #maybe want to add delay from first puts to second, maybe 3 seconds.
+    puts "Choose your destiny"
+    popular_martial_arts
   end
 
 end
