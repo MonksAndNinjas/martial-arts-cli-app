c139b68b2381608f727f584ab0168b2b81950463
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-20 22:00:06 +0000

    added menu method

--- lib/martial_arts/cli.rb
+++ lib/martial_arts/cli.rb
@@ -5,7 +5,16 @@ class MartialArts::CLI
     puts "OOOOOOSSSsss"
     #maybe want to add delay from first puts to second, maybe 3 seconds.
     puts "Choose your destiny"
-    popular_martial_arts
+    #lists menu such as popular martial arts, martial arts by country, focus, and style
+    menu
+  end
+
+  def menu
+    puts "1. Top 10 martial arts type"
+    puts "2. Martail Arts by Country"
+    puts "3. Martial Art Styles"
+    puts "4. Martial Art by Fighting-Focus"
+    puts "5. quit"
   end
 
 end
