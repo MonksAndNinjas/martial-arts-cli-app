38bac251657f9eda7881d6266fe6bf983a5164d4
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-20 23:10:11 +0000

    Changed call and menu method. Difficult to read.

--- lib/martial_arts/cli.rb
+++ lib/martial_arts/cli.rb
@@ -5,42 +5,34 @@ class MartialArts::CLI
     puts "OOOOOOSSSsss"
     #maybe want to add delay from first puts to second, maybe 3 seconds.
     puts "Choose your destiny"
-    #lists menu such as popular martial arts, martial arts by country, focus, and style
-    menu
-    #gets input from user
-    input = gets.chomp.to_i
 
-    return_request(input)
-    until input < 6 and input > 0
-      return_request(gets.chomp.to_i)
-    end
-    #double check until loop when have created methods for menu options
-  end
-
-  def menu
     puts "1. Popular Martial Arts"
     puts "2. Martail Arts by Country"
     puts "3. Martial Art Styles"
     puts "4. Martial Art by Fighting-Focus"
     puts "5. quit"
     puts "Please enter the corresponding number" #should I give example of what to type
+
+    menu
   end
 
-  def return_request(input)
-    if input == 1
-      popular_martial_arts
-    elsif input == 2
-      martial_art_by_country
-    elsif input == 3
-      martial_art_styles
-    elsif input == 4
-      martial_art_by_focus
-    elsif input == 5
-      quit
-    else
-      puts "invalid entry"
+  def menu
+      input = nil
+    while input != 5
+      input = gets.chomp.to_i
+      case input
+      when 1
+        puts "martial arts method"
+      when 2
+        puts "Select amongst the listed countries method"
+      when 3
+        puts "List of all styles available method"
+      when 4
+        puts "List of martial arts by fighting-focus method"
+      else
+        puts "I'm confused, can you try that again?"
+      end
     end
   end
-
 
 end
