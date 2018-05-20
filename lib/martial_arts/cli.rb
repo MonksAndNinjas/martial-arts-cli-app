5df02b9aef25b3ed18bc1c6cc981d5d126f5d9e6
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-20 22:23:28 +0000

    added return-request method

--- lib/martial_arts/cli.rb
+++ lib/martial_arts/cli.rb
@@ -14,12 +14,29 @@ class MartialArts::CLI
   end
 
   def menu
-    puts "1. Top 10 martial arts"
+    puts "1. Popular Martial Arts"
     puts "2. Martail Arts by Country"
     puts "3. Martial Art Styles"
     puts "4. Martial Art by Fighting-Focus"
     puts "5. quit"
     puts "Please enter the corresponding number" #should I give example of what to type
+  end
+
+  def return_request(input)
+      if input == 1
+        popular_martial_arts
+      elsif input == 2
+        martial_art_by_country
+      elsif input == 3
+        martial_art_styles
+      elsif input == 4
+        martial_art_by_focus
+      elsif input == 5
+        quit
+      end
+        puts "invalid entry"
+    end
+
   end
 
 end
