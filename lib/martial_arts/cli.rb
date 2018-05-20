9dca804fb5461faf1e77ffe1377f8e867c565b10
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-20 22:09:12 +0000

    changes to call method

--- lib/martial_arts/cli.rb
+++ lib/martial_arts/cli.rb
@@ -7,14 +7,19 @@ class MartialArts::CLI
     puts "Choose your destiny"
     #lists menu such as popular martial arts, martial arts by country, focus, and style
     menu
+    #gets input from user
+    input = gets.chomp.to_i
+    #checks for appropriate input then returns request
+    return_request(input)
   end
 
   def menu
-    puts "1. Top 10 martial arts type"
+    puts "1. Top 10 martial arts"
     puts "2. Martail Arts by Country"
     puts "3. Martial Art Styles"
     puts "4. Martial Art by Fighting-Focus"
     puts "5. quit"
+    puts "Please enter the corresponding number" #should I give example of what to type
   end
 
 end
