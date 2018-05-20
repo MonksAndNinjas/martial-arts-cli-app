502c90ba51740dd5d310c39a2b19b61dd3d800bf
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-20 23:29:58 +0000

    added notes

--- lib/martial_arts/cli.rb
+++ lib/martial_arts/cli.rb
@@ -6,29 +6,34 @@ class MartialArts::CLI
     #maybe want to add delay from first puts to second, maybe 3 seconds.
     puts "Choose your destiny"
 
-    puts "1. Popular Martial Arts"
-    puts "2. Martail Arts by Country"
-    puts "3. Martial Art Styles"
-    puts "4. Martial Art by Fighting-Focus"
-    puts "5. quit"
-    puts "Please enter the corresponding number" #should I give example of what to type
-
     menu
   end
 
   def menu
-      input = nil
-    while input != 5
-      input = gets.chomp.to_i
+    input = nil
+
+    while input != "exit"
+      #navigation
+      puts "1. Popular Martial Arts"
+      puts "2. Martail Arts by Country"
+      puts "3. Martial Art Styles"
+      puts "4. Martial Art by Fighting-Focus"
+      puts "exit"
+      puts "Please enter the corresponding number or type exit"
+      #gets user input
+      input = gets.strip.downcase
+      #waits for response and then returns request
       case input
-      when 1
+      when "1"
         puts "martial arts method"
-      when 2
+      when "2"
         puts "Select amongst the listed countries method"
-      when 3
+      when "3"
         puts "List of all styles available method"
-      when 4
+      when "4"
         puts "List of martial arts by fighting-focus method"
+      when "exit"
+        puts "Goodbye"
       else
         puts "I'm confused, can you try that again?"
       end
