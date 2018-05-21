2793fbeb6cc73539c2f3b998fbd3621a9bcd0a28
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-21 19:36:18 +0000

    Added to all_styles_list method in cli

--- lib/martial_arts/cli.rb
+++ lib/martial_arts/cli.rb
@@ -28,7 +28,7 @@ class MartialArts::CLI
       when "2"
         martial_arts_by_country_submenu
       when "3"
-        all_styles
+        all_styles_submenu
       when "4"
         puts "List of martial arts by fighting-focus method"
       when "exit"
@@ -93,7 +93,34 @@ class MartialArts::CLI
     end
   end
 
-  def
+  def all_styles_submenu
+    all_styles_list
+    size = MartialArts::Styles.all.size
+
+    input = nil
+
+    while input != "back"
+      input = gets.strip.downcase
+
+      case input
+      when /\d\d*/ #Checks if string is a number
+        if (1...size+1).include?(input.to_i)
+          puts "style.name"
+          puts "style.country"
+          puts "style.fighting_focus"
+          puts "style.description"
+
+          puts "Type list, or back"
+        end
+      when "list"
+        all_styles_list
+      when "back"
+        break
+      else
+        puts "I'm confused, can you try that again?"
+      end
+    end
+  end
 
   def popular_list
     MartialArts::Styles.todays_list.each.with_index(1) do |style_instance, i|
@@ -156,6 +183,15 @@ class MartialArts::CLI
 
     puts "Enter the corresponding number for more information about the style"
     puts "Otherwise type back or list"
+  end
+
+  def all_styles_list
+    MartialArts::Styles.all.each.with_index(1) do |style_instance, i|
+      puts "#{i}. #{style_instance}"  #when setup change to style_instance.name
+    end
+
+    puts  "Enter the corresponding number for more information on the style of martial art."
+    puts  "Otherwise, type back or list"
   end
 
 end
--- lib/martial_arts/styles.rb
+++ lib/martial_arts/styles.rb
@@ -5,6 +5,7 @@ class MartialArts::Styles
   def self.todays_list
     ["style0", "style1", "style2", "style3", "styel4", "style5", "style6", "style7", "style8", "style9"]
     #scrape from website
+  end
 
   def self.all
     ["style0", "style1", "style2", "style3", "styel4", "style5", "style6", "style7", "style8", "style9"]
