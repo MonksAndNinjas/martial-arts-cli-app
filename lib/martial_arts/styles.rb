725c9ce331bec230da1ccda47cc063c5ed5cf71b
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-21 19:15:07 +0000

    added self.all method to read @@all = []

--- lib/martial_arts/styles.rb
+++ lib/martial_arts/styles.rb
@@ -1,8 +1,14 @@
 
 class MartialArts::Styles
+  @@all = []
 
   def self.todays_list
     ["style0", "style1", "style2", "style3", "styel4", "style5", "style6", "style7", "style8", "style9"]
+    #scrape from website
+
+  def self.all
+    ["style0", "style1", "style2", "style3", "styel4", "style5", "style6", "style7", "style8", "style9"]
+    #use array @@all
   end
 
 end
