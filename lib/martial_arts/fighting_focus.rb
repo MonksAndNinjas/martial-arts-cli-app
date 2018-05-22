bd338a75815e290143cb7daea2cb19679e8f4d92
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-22 05:45:16 +0000

    added puts for all fighting type methods

--- lib/martial_arts/fighting_focus.rb
+++ lib/martial_arts/fighting_focus.rb
@@ -2,18 +2,23 @@
 class MartialArts::FightingFocus
 
   def self.striking
+    puts "I am a striking list"
   end
 
   def self.grappling
+    puts "I am a grappling list"
   end
 
   def self.hybrid
+    puts "I am a hybrid list"
   end
 
   def self.weaponry
+    puts "I am a weaponry list"
   end
 
   def self.meditative
+    puts "I am a meditative list"
   end
-  
+
 end
