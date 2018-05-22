a1fd01fea844c7bd36f4120964b3a1fae53226e7
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-22 05:43:40 +0000

    added fighting type methods

--- lib/martial_arts/fighting_focus.rb
+++ lib/martial_arts/fighting_focus.rb
@@ -1,6 +1,19 @@
 
 class MartialArts::FightingFocus
 
+  def self.striking
+  end
 
+  def self.grappling
+  end
 
+  def self.hybrid
+  end
+
+  def self.weaponry
+  end
+
+  def self.meditative
+  end
+  
 end
