de8ff7091b44f5b1fc6f072139542a6284638a0f
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-23 17:17:00 +0000

    added doc in scrape_countries method

--- lib/martial_arts/countries.rb
+++ lib/martial_arts/countries.rb
@@ -9,7 +9,8 @@ class MartialArts::Countries
     self.scrape_countries
   end
 
-  def self.scrpe_countries
+  def self.scrape_countries
+    doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_martial_arts"))
     ["country0", "country1", "country2", "country3", "country4", "country5", "country6", "country7", "country8", "country9"]
   end
 
