f5d0d2ac19a6a5ef3dd1d39d8df1e8925c8ac15e
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-23 18:59:36 +0000

    scraped countries

--- lib/martial_arts/countries.rb
+++ lib/martial_arts/countries.rb
@@ -11,6 +11,18 @@ class MartialArts::Countries
 
   def self.scrape_countries
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_martial_arts"))
+    africa = doc.css('.div-col.columns.column-width')[0].css('ul') #add [i].text
+    americas = doc.css('.div-col.columns.column-width')[1].css('dt a') #add .text
+    asia = doc.css('.div-col.columns.column-width')[2].css('dt a')  #add .text
+    europe = doc.css('.div-col.columns.column-width')[4].css('dt a')  #add .text
+
+    countries = doc.css('.div-col.columns.column-width').each_with_index.collect do |country, i|
+                  country.css('dt a').text if [0,3,5].include?(i) == false
+                end.compact
+
+
+
+    binding.pry
     ["country0", "country1", "country2", "country3", "country4", "country5", "country6", "country7", "country8", "country9"]
   end
 
