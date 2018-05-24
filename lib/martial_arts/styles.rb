75277a456991d6f321170cbdb8ba478d74ba7fe9
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-05-24 17:57:26 +0000

    sorted styles list from instances

--- lib/martial_arts/styles.rb
+++ lib/martial_arts/styles.rb
@@ -17,9 +17,9 @@ class MartialArts::Styles
   def self.scrape_styles
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_martial_arts"))
     doc.css('.div-col.columns.column-width').css('li').each do |style|
-      valid?
-      style_instance = MartialArts::Styles.new(style.css('a')[0].text)
-
+      if self.valid?(style.css('a')[0].text) == nil
+        self.all << MartialArts::Styles.new(style.css('a')[0].text)  #saves instance
+      end
     end
   end
 
@@ -32,15 +32,21 @@ class MartialArts::Styles
   end
 
   def self.styles_list
-    self.all.each.with_index(1) do |style, i|
-      puts "#{i}. #{style.name}"  #when setup change to style_instance.name
+    #displays list of all styles
+    sorted_styles = self.all.sort {|a,b| a.name <=> b.name }
+    sorted_styles.each.with_index(1) do |style, i|
+      puts "#{i}. #{style.name}"
     end
 
     puts  "Enter the corresponding number for more information on the style of martial art."
     puts  "Otherwise, type back or list"
   end
 
-  def valid?
+  def self.valid?(style)
+    #checks for duplicates
+    self.all.detect do |style_instance|
+      style_instance.name == style
+    end
   end
 
 end
